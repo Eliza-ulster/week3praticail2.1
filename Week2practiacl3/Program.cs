bee5de9b4ca267a833289de7d46d5e33061a2bdc
Eliza Ann Benoy <Eliza_Ann_Benoy-EAB@ulster.ac.uk>
2024-10-16 20:16:51 +0100

    Task 6

--- Week2practiacl3/Program.cs
+++ Week2practiacl3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Main();
+
+static void Main()
+{
+    Console.WriteLine("Enter a string:");
+    string str = Console.ReadLine();
+
+    Console.WriteLine("Enter no of rotation:");
+    int noOfrotation = Convert.ToInt32(Console.ReadLine());
+    
+
+    string encrypt = (Encrypt(str, noOfrotation));
+    Console.WriteLine($"The sentence you inputted is :, {str}");
+    Console.WriteLine($"the encrypted sentence is now :, {encrypt}");
+
+   
+    
+}
+static string Encrypt(string ToEncrpy, int Rotation)
+{
+    string encodedText = " ";
+    foreach(char c in ToEncrpy)
+    {
+        int encodedChar = c + Rotation;
+        
+        encodedText += (char)encodedChar;
+
+    }
+
+    return encodedText;
+
+}
